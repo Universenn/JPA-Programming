062cfdcffdcb3678c159e1681560a511f2414bd8
Universenn <dnwndhks98@gmail.com>
2023-07-03 17:50:26 +0900

    [feat] 실습 : 상속관계 매핑 단일 전략

--- src/main/java/com/example/jpaprogramming/hellojpa/domain3/Item.java
+++ src/main/java/com/example/jpaprogramming/hellojpa/domain3/Item.java
@@ -8,8 +8,9 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@Inheritance(strategy = InheritanceType.JOINED)
-// 기본은 default 값은 : SINGLE_TABLE
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+// insert 쿼리도 한번에 조인 할 필요도 없어서 성능은 가장 잘 나온다
+// 싱글 전략은 DTYPE 이 자동으로 생성된다. 구분할 방법이 없기 떄문에
 @DiscriminatorColumn
 // DTYPE 생성
 public class Item {
